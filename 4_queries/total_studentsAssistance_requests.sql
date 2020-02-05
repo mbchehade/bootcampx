b7c418377749eee39399f126b73cac9244478ff2
mbchehade <57118552+mbchehade@users.noreply.github.com>
2020-02-04 17:16:02 -0700

    average of assistance time from creation to start to completion

---
+++ 4_queries/total_studentsAssistance_requests.sql
@@ -0,0 +1,11 @@
+-- Get the total number of assistance_requests for a student.
+
+-- Select the student
+-- 's name and the total assistance requests.
+-- Since this query needs to work with any specific student name, use 'Elliot Dickinson' for the student's name here.
+
+SELECT count(assistance_requests.*) as total_assistances, students.name
+FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
