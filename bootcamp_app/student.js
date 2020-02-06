aa6c05ca5d114586a8cf4cceebd0c2b3aec54d1f
moe <mbchehade@gmail.com>
2020-02-06 01:17:10 +0000

    queriying the database

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-
+//Pool is the preferred way to manage client connections becacuse POOL will
+//manage multiple clinet connection for us
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,15 +8,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+//Here we are going to start querying the database
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
     })
-  }).catch(err => console.error('query error', err.stack));
+  })
+  .catch(err => console.error('query errpr', err.stack));
